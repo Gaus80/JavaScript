dca7dcf985707537c640e3ce626c4be487d25339
Stiven <usgarzon@ucundinamarca.edu.co>
2025-01-01 10:19:01 -0500

    palindromo

--- FreeCode/section1/04Palindromo/script.js
+++ FreeCode/section1/04Palindromo/script.js
@@ -1,35 +1,48 @@
 let input = document.getElementById("text-input");
 let resultado = document.getElementById("result");
 let boton = document.getElementById("check-btn");
-let arreglo = [];
-let arreglo2 = [];
+
 
 function validar(palabra){
+    let arreglo = [];
+    let arreglo2 = [];
     const palabraLower = palabra.toLowerCase();
     let regex = /[,./()_-\s]/g;
     const nuevaPalabra = palabraLower.replace(regex,"");
-
     for(const letra of nuevaPalabra){
         arreglo.push(letra)
         arreglo2.unshift(letra);
     }
     let esPalindromo = true;
-
     for(let i = 0; i < arreglo.length;i++){
         if(arreglo[i] !== arreglo2[i]){
             esPalindromo = false;
         }
     }
-
     if (esPalindromo){
         return true;
     }else{
         return false;
     }
-
 }
 
+boton.addEventListener("click",()=>{
 
-console.log(validar("never odd or even"))
-console.log(arreglo);
-console.log(arreglo2)
+    if(input.value ===""){
+        alert("Please input a value")
+    }else{
+        resultado.style.display = "block";
+        if(validar(input.value)){
+            resultado.innerText = `${input.value} is a palindrome`
+        }
+        else{
+            resultado.innerText = `${input.value} is not a palindrome`
+        }
+        
+    }
+
+})
+  ;
+ 
+
+
