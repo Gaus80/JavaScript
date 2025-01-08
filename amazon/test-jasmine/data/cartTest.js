bcf26fe2f4a3aea8824b82bfbe96c2b9e288325c
Stiven <usgarzon@ucundinamarca.edu.co>
2025-01-08 11:00:35 -0500

    Create automated for add-to-cart

---
+++ amazon/test-jasmine/data/cartTest.js
@@ -0,0 +1,42 @@
+import { cart,addToCart, loadFromStorage  } from '../../data/cart.js';
+
+describe('test suite: add to the cart',() => {
+    it('adds an existing product to the cart', () =>{
+        spyOn(localStorage,'setItem');
+
+        spyOn(localStorage, 'getItem').and.callFake(()=>{
+            return JSON.stringify([{
+                productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+                quantity:1,
+                deliveryOptionId:'1'
+            }]);
+        });
+        loadFromStorage();
+
+        addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e')
+        expect(cart.length).toEqual(1)
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+        expect(cart[0].quantity).toEqual(2);
+
+    });
+    
+    it('adds a new product to the cart', () =>{
+//MOCK JASMINE WITH A FAKE CALL
+        spyOn(localStorage,'setItem');
+
+        spyOn(localStorage, 'getItem').and.callFake(()=>{
+            return JSON.stringify([])
+        });
+
+        loadFromStorage();
+
+        addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e')
+        expect(cart.length).toEqual(1)
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+        expect(cart[0].quantity).toEqual(1);
+    });
+  
+});
+
